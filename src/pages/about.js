dd6fea6736ef414ee4bdf8da90794b88fe1fb7fe
Brandon <brandongines@gmail.com>
2020-03-06 00:55:06 -0500

    worl

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import PrimaryLayout from "../layouts/PrimaryLayout"
 export default () => (
   <PrimaryLayout column="col-xs-6">
-    <h1>About Us</h1>
+    <h1>About me</h1>
     <p>
       Departure so attention pronounce satisfied daughters am. But shy tedious
       pressed studied opinion entered windows off. Advantage dependent suspicion
